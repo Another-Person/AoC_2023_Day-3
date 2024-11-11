aeaeda4d76bd66f8c089b2861ff04c7d1d94d0e3
Joey Sachtleben <manjoey7@gmail.com>
2024-11-11 12:08:37 -0600

    General code cleanup

--- AoC_2023_Day-3.cpp
+++ AoC_2023_Day-3.cpp
@@ -21,11 +21,9 @@
 
 namespace fs = std::filesystem;
 
-using std::cout;
 using std::cerr;
-using std::exception;
+using std::cout;
 using std::ranges::for_each;
-using std::ifstream;
 using std::isdigit;
 using std::ispunct;
 using std::invalid_argument;
@@ -120,11 +118,15 @@ void FindValidDigits(NodeGrid& grid)
 	{
 		for (size_t y = 0; y < grid.at(x).size(); ++y)
 		{
+			// Validate that we're at a digit before processing
 			Node& node = grid.at(x).at(y);
 			if (!isdigit(node.getCharacter()))
 			{
 				continue;
 			}
+
+			// Go through and check all surrounding nodes to see if they have a special character (AKA punctuation other than .)
+			// If they do, mark the node and move on to the next.
 
 			if (x != 0)
 			{
@@ -233,14 +235,18 @@ void FindValidNumbers(NodeGrid& grid)
 	{
 		for (size_t y = 0; y < grid.at(x).size(); ++y)
 		{
+			// Find a node which *IS* near a special character but has *NOT* been already used in a number.
+			// If either of these conditions is not met, move on to the next node.
 			Node& node = grid.at(x).at(y);
 			if (!node.isNearSpecial() || node.hasBeenUsed())
 			{
 				continue;
 			}
 
+			// Since we now know we are at a digit that's part of a valid number, mark it as such...
 			node.use();
 
+			// ...and then proceed to find any digits that are to the left or right of this digit and mark them as being part of a number.
 			for (int r_y = y-1; r_y >= 0; --r_y)
 			{
 				Node& back = grid.at(x).at(r_y);
@@ -284,18 +290,21 @@ void FindValidNumbers(NodeGrid& grid)
  */
 void CreateValidNumbers(const NodeGrid& grid, vector<int>& numbers)
 {
-	cout << "Creating valid numbers...";
+	cout << "Creating valid numbers... ";
 	cout.flush();
 
 	for (size_t x = 0; x < grid.size(); ++x)
 	{
 		for (size_t y = 0; y < grid.at(x).size(); ++y)
 		{
+			// Find a digit that's marked as being in a number.
+			// Since we're iterating left to right, top to bottom, we know that we'll always hit the first digit, well, first.
 			if (!grid.at(x).at(y).hasBeenUsed())
 			{
 				continue;
 			}
 
+			// Starting at that digit, go through and find all connected digits to add them to a string
 			string rawNum;
 			while (y < grid.at(x).size())
 			{
@@ -311,11 +320,60 @@ void CreateValidNumbers(const NodeGrid& grid, vector<int>& numbers)
 				y++;
 			}
 
+			// Add the resulting number to the vector
 			numbers.emplace_back(std::stoi(rawNum));
 		}
 	}
 
 	cout << "done.\n";
+}
+
+/* CreateNodes
+ *	Fills a NodeGrid with nodes from a specified path.
+ *
+ * Parameters:
+ *	path - a path to a file to be read. It is assumed the path has been validated by ValidatePath.
+ *	nodes - an empty NodeGrid to be filled.
+ *
+ * Returns:
+ *	Nothing. The NodeGrid is modified in place.
+ *
+ * Throws:
+ *	runtime_error if unable to properly open path.
+ *	May throw out_of_range.
+ */
+void CreateNodes(const fs::path& path, NodeGrid& nodes)
+{
+	std::ifstream inputFile(path);
+
+	if (!inputFile.is_open())
+	{
+		ss msg;
+		msg << "Unable to open file \"" << path.string() << "\".\n";
+		throw std::runtime_error(msg.str());
+	}
+
+	nodes.emplace_back();
+	while (!inputFile.eof())
+	{
+		const char c = static_cast<char>(inputFile.get());
+
+		if (c == std::char_traits<char>::eof())
+		{
+			break;
+		}
+
+		// Only add a new row if this isn't the end of the file
+		if (c == '\n')
+		{
+			if (inputFile.peek() != std::char_traits<char>::eof())
+			{
+				nodes.emplace_back();
+			}
+			continue;
+		}
+		nodes.at(nodes.size()-1).emplace_back(c);
+	}
 }
 
 int main(int argc, char* argv[])
@@ -326,52 +384,26 @@ int main(int argc, char* argv[])
 
 	if (argc == 2 && argv[1][0] != NULL)
 	{
-		cout << "Using input file from command line \"" << argv[1] << "\n";
+		cout << "Using input file from command line \"" << argv[1] << "\".\n";
 		pathName = argv[1];
 	}
 	else
 	{
-		cout << "Using default file path " << pathName << "\n";
+		cout << "Using default file path \"" << pathName << "\".\n";
 	}
 
-	//try
-	//{
+	try
+	{
 		fs::path path = ValidatePath(pathName);
-
-		ifstream inputFile(path);
-
-		if (!inputFile.is_open())
-		{
-			ss msg;
-			msg << "Unable to open file \"" << path.string() << "\".\n";
-			throw std::runtime_error(msg.str());
-		}
-
 		NodeGrid nodes;
-		nodes.emplace_back();
-		while (!inputFile.eof())
-		{
-			const char c = static_cast<char>(inputFile.get());
 
-			if (c == -1)
-			{
-				break;
-			}
-
-			if (c == '\n')
-			{
-				nodes.emplace_back();
-				continue;
-			}
-			nodes.at(nodes.size()-1).emplace_back(c);
-		}
-
+		CreateNodes(path, nodes);
 		FindValidDigits(nodes);
 
-		const auto total = for_each(nodes, RowCount()).fun;
+		const auto [nodesTotal, validSum] = for_each(nodes, RowCount()).fun;
 
-		cout << "Created " << total.sum << " nodes in " << nodes.size() << " rows!\n";
-		cout << "Found " << total.validSum << " digits near a special character!\n";
+		cout << "Created " << nodesTotal << " nodes in " << nodes.size() << " rows!\n";
+		cout << "Found " << validSum << " digits near a special character!\n";
 
 		FindValidNumbers(nodes);
 		vector<int> numbers;
@@ -379,8 +411,8 @@ int main(int argc, char* argv[])
 		int sum = std::accumulate(numbers.begin(), numbers.end(), 0);
 		cout << "Found " << numbers.size() << " valid numbers totalling to " << sum << "!\n";
 		
-	/*}
-	catch (exception &e)
+	}
+	catch (std::exception &e)
 	{
 		cerr << "Caught exception: " << e.what() << "\n";
 		retval = 1;
@@ -389,7 +421,7 @@ int main(int argc, char* argv[])
 	{
 		cerr << "Unknown exception encountered.\n";
 		retval = 1;
-	}*/
+	}
 
 	return retval;
 }
